237df4c34f7906e655bb29217b9cae49abf6dd9b
Luis Miguel Torres Villegas <lmtorresv@eafit.edu.co>
2024-03-01 12:45:20 -0500

    agregar figura de barras de películas por año

--- movie/views.py
+++ movie/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 
 from .models import Movie
 
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import urllib, base64
+
 # Create your views here.
 
 def home(request):
@@ -15,3 +20,39 @@ def home(request):
 
 def about(request):
   return render(request, "about.html")
+
+def statistics_view(request):
+  matplotlib.use("Agg")
+  years = Movie.objects.values_list("year", flat=True).distinct().order_by("year")
+  movie_counts_by_year = {}
+  for year in years:
+    if year:
+      movies_in_year = Movie.objects.filter(year=year)
+    else:
+      movies_in_year = Movie.objects.filter(year__isnull=True)
+      year = "None"
+    movie_counts_by_year[year] = movies_in_year.count()
+
+  bar_width = 0.5
+  bar_spacing = 0.5
+  bar_positions = range(len(movie_counts_by_year))
+
+  plt.bar(bar_positions, movie_counts_by_year.values(), width=bar_width)
+  plt.title("Movies per year")
+  plt.xlabel("Year")
+  plt.ylabel("Number of movies")
+  plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation=90)
+
+  plt.subplots_adjust(bottom=0.3)
+
+  buffer = io.BytesIO()
+  plt.savefig(buffer, format="png")
+  buffer.seek(0)
+  plt.close()
+
+  image_png = buffer.getvalue()
+  buffer.close()
+  graphic = base64.b64encode(image_png)
+  graphic = graphic.decode("utf-8")
+
+  return render(request, "statistics.html", {"graphic": graphic})
